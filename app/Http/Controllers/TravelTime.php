9b5a4138b581c74cb66e7896b84c419669e1bbb4
Robert Crous <robert.crous21@gmail.com>
2020-04-05 16:07:12 +0200

    Add logic in Travel Time controller to use Google Maps service

--- app/Http/Controllers/TravelTime.php
+++ app/Http/Controllers/TravelTime.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Services\GoogleMaps;
 
 class TravelTime extends Controller
 {
     public function get(Request $request, GoogleMaps $google_maps) {
+        // always deal in lower case, there are cleaner ways of handling this
+        // that I should probably do instead but this is dragging on :/
+        $suburbs = array_map('strtolower', explode(',', $request->suburbs));
+        $result = $google_maps->getTravelTimesFor($suburbs);
+
+        return response()->json($result);
     }
 }
